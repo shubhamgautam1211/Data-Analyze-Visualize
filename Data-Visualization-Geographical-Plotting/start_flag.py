d09fe502a555b8aa6fa5fec61abf1c62dbb09287
Shubham Gautam <57455438+shubhamgautam1211@users.noreply.github.com>
2021-06-04 00:13:31 +0530

    Create start_flag.py

---
+++ Data-Visualization-Geographical-Plotting/start_flag.py
@@ -0,0 +1,29 @@
+
+class Node:
+
+    def __init__(self, name):
+        self.name = name
+        self.adjacency_list = []
+        self.visited = False
+
+
+def depth_first_search(node):
+
+    node.visited = True
+    print(node.name)
+
+    for n in node.adjacency_list:
+        if not n.visited:
+            depth_first_search(n)
+
+
+if __name__ == '__main__':
+
+    # first we have to create the vertices (nodes)
+    node1 = Node("A")
+    node2 = Node("B")
+    node3 = Node("C")
+    node4 = Node("D")
+    node5 = Node("E")
+
+
