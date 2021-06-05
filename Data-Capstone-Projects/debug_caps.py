924c068ec255fc3706dc906bbeb382c3feb69acb
Shubham Gautam <57455438+shubhamgautam1211@users.noreply.github.com>
2021-06-06 00:12:10 +0530

    Update debug_caps.py

--- Data-Capstone-Projects/debug_caps.py
+++ Data-Capstone-Projects/debug_caps.py
@@ -27,3 +27,13 @@ def depth_first_search(start_node):
                 stack.append(n)
 
 
+if __name__ == '__main__':
+
+    # first we have to create the vertices (nodes)
+    node1 = Node("A")
+    node2 = Node("B")
+    node3 = Node("C")
+    node4 = Node("D")
+    node5 = Node("E")
+
+    
