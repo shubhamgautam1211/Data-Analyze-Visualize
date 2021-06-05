3b22296b16d57aaabda571d8c984eaa066b42b76
Shubham Gautam <57455438+shubhamgautam1211@users.noreply.github.com>
2021-06-06 00:12:36 +0530

    Update debug_caps.py

--- Data-Capstone-Projects/debug_caps.py
+++ Data-Capstone-Projects/debug_caps.py
@@ -33,6 +33,13 @@ if __name__ == '__main__':
     node1 = Node("A")
     node2 = Node("B")
     node3 = Node("C")
+    
+      node1.adjacency_list.append(node3)
+    node2.adjacency_list.append(node4)
+    node4.adjacency_list.append(node5)
+
+    # run the DFS
+    depth_first_search(node1)
     node4 = Node("D")
     node5 = Node("E")
     
